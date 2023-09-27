0bf530e6b47921087e8c6744918c30aa4bbfdd82
Paul Weissenbach <paul.weissenbach@posedio.com>
2023-09-27 15:58:15 +0200

    Fake call the service method from grcp controller

--- src/grpc.controller.ts
+++ src/grpc.controller.ts
@@ -10,25 +10,31 @@ export class GrpcController {
   constructor(private readonly appService: AppService) {}
 
   @GrpcMethod('serviceofferingPublisher')
-  publishOfferingJson(data: JsonOffering): Status {
+  async publishOfferingJson(data: JsonOffering): Promise<Status> {
     this.logger.debug('grpc method publishOfferingJson called');
 
     console.log(data.metadata);
 
+    // TODO: use the right input...
+    await this.appService.publishEverything(data.metadata);
+
+    // TODO: what's the response
     return {
-      statusCode: 2,
-      simpleMessage: 'Not sure',
+      statusCode: 201,
+      simpleMessage: 'CREATED',
       DebugInformation: undefined,
     };
   }
 
   @GrpcMethod('serviceofferingPublisher')
-  publishOffering(data: Offering): Status {
+  async publishOffering(data: Offering): Promise<Status> {
     console.log(data);
 
+    await this.appService.publishEverything(data.main);
+
     return {
-      statusCode: 2,
-      simpleMessage: 'Not sure',
+      statusCode: 201,
+      simpleMessage: 'CREATED',
       DebugInformation: undefined,
     };
   }
