d7921343b16c749d576be8e76ac499a709f27d8a
Sebastian Waldbauer <sebastian.waldbauer@posedio.com>
2024-06-05 13:53:09 +0200

    fix: remove rest http service
    Signed-off-by: Sebastian Waldbauer <sebastian.waldbauer@posedio.com>

--- src/main.ts
+++ src/main.ts
@@ -10,25 +10,12 @@ async function bootstrap() {
   app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.GRPC,
     options: {
-      url: '0.0.0.0:5000',
+      url: '0.0.0.0:8989',
       package: 'eupg.serviceofferingpublisher',
       protoPath: join(__dirname, './_proto/spp.proto'),
     },
   });
 
   await app.startAllMicroservices();
-
-  const config = new DocumentBuilder()
-    .setTitle('Publishing Connector')
-    .setDescription(
-      'The Connectors API description. It pushes Verifiable Credentials for Gaia-X to different catalogs/marketplaces',
-    )
-    .setVersion('0.1.0')
-    .addTag('publishing-connector')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
-
-  await app.listen(3000);
 }
 bootstrap();
