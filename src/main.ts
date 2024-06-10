b277ef4079e906f8490249fff7d0b93ee1d5952d
Sebastian Waldbauer <sebastian.waldbauer@posedio.com>
2024-06-10 11:16:58 +0200

    chore: remove unused imports
    Signed-off-by: Sebastian Waldbauer <sebastian.waldbauer@posedio.com>

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { join } from 'path';
