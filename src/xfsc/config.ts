5e32d7400a9703848313c4fe63c71348309b5822
Julian Reichelt <reichelt@brinkhaus-gmbh.de>
2024-07-25 22:01:34 +0200

    Add XFSC CAT url and token endpoint to config.ts

--- src/xfsc/config.ts
+++ src/xfsc/config.ts
@@ -2,3 +2,5 @@ require('dotenv').config()
 
 export const XFSC_USERNAME = process.env.XFSC_USERNAME
 export const XFSC_PASSWORD = process.env.XFSC_PASSWORD
+export const XFSC_CAT_TOKEN_ENDPOINT = process.env.XFSC_CAT_TOKEN_ENDPOINT
+export const XFSC_CAT_HOST = process.env.XFSC_CAT_HOST
