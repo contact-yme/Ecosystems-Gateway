0a12229df1a1e536ec2648607ef2ff08b4b8d89e
Julian Reichelt <reichelt@brinkhaus-gmbh.de>
2024-07-30 21:04:53 +0200

    Do tiny change to .env vars

--- src/xfsc/config.ts
+++ src/xfsc/config.ts
@@ -3,4 +3,4 @@ require('dotenv').config()
 export const XFSC_USERNAME = process.env.XFSC_USERNAME
 export const XFSC_PASSWORD = process.env.XFSC_PASSWORD
 export const XFSC_CAT_TOKEN_ENDPOINT = process.env.XFSC_CAT_TOKEN_ENDPOINT
-export const XFSC_CAT_HOST = process.env.XFSC_CAT_HOST
+export const XFSC_CAT_HOST_SD_ENDPOINT = process.env.XFSC_CAT_HOST_SD_ENDPOINT
