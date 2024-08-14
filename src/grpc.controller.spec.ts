3fd77f9182d29b7c180f828204d564d2de7b99e0
Sebastian Waldbauer <sebastian.waldbauer@posedio.com>
2024-08-14 10:46:30 +0200

    test: add compute to data

--- src/grpc.controller.spec.ts
+++ src/grpc.controller.spec.ts
@@ -5,9 +5,12 @@ import {
   PontusxOffering,
   Pricing_PricingType,
 } from './generated/src/_proto/spp_v2';
+import { CreateComputeToDataRequest, CreateComputeToDataResultRequest, GetComputeToDataResultResponse, GetOfferingRequest } from './generated/src/_proto/spp';
+import { RpcException } from '@nestjs/microservices';
 
 describe('Grpc Controller', () => {
   let controller: GrpcController;
+  let pontusxService: PontusxService;
 
   const mockPontusXService = {
     publishAsset: jest.fn(() => ({
@@ -15,6 +18,7 @@ describe('Grpc Controller', () => {
     })),
     updateOffering: jest.fn(),
     updateOfferingLifecycle: jest.fn(),
+    getOffering: jest.fn()
   };
 
   beforeEach(async () => {
@@ -23,12 +27,20 @@ describe('Grpc Controller', () => {
       providers: [
         {
           provide: PontusxService,
-          useValue: mockPontusXService,
+          useValue: {
+            publishComputeAsset: jest.fn(),
+            updateOffering: jest.fn(),
+            setState: jest.fn(),
+            requestComputeToData: jest.fn(),
+            getOffering: jest.fn(),
+            getComputeToDataResult: jest.fn()
+          },
         },
       ],
     }).compile();
 
     controller = module.get<GrpcController>(GrpcController);
+    pontusxService = module.get<PontusxService>(PontusxService);
   });
 
   it('should be defined', () => {
@@ -37,39 +49,83 @@ describe('Grpc Controller', () => {
 
   it('Create offering', async () => {
     const result = await controller.createOffering({
-      offerings: [
-        {
-          pontusxOffering: {
-            metadata: {
-              type: 'dataset',
-              name: '',
-              author: '',
-              licence: '',
-              tags: [],
-              description: '',
-            },
-            services: [
-              {
-                type: 'access',
-                files: [
-                  {
-                    url: '',
-                    method: '',
-                  },
-                ],
-                pricing: {
-                  pricingType: Pricing_PricingType.FREE,
-                },
-                consumerParameters: [],
-              },
-            ],
-            additionalInformation: undefined,
-          },
-        },
-      ],
+      main: {
+        type: 'dataset',
+        author: '',
+        licence: '',
+        dateCreated: '',
+        files: [],
+        tags: [],
+        description: '',
+        allowedAlgorithm: [],
+      },
+      additionalInformation: undefined,
+      token: '',
+      name: '',
     });
 
     expect(mockPontusXService.publishAsset.mock.calls).toHaveLength(1);
     expect(result.id[0]).toEqual('test-id');
+  });
+
+  describe('runComputeToData', () => {
+    it('should return a jobId if successful', async () => {
+      const mockData: CreateComputeToDataRequest = { did: 'testDid', algorithm: 'testAlgorithm' } as any;
+      const mockResult = ['mockJobId'];
+
+      jest.spyOn(pontusxService, 'requestComputeToData').mockResolvedValue(mockResult);
+
+      const result = await controller.runComputeToData(mockData);
+
+      expect(result).toEqual({ jobId: mockResult });
+      expect(pontusxService.requestComputeToData).toHaveBeenCalledWith(mockData.did, mockData.algorithm);
+    });
+
+    it('should throw an RpcException if requestComputeToData fails', async () => {
+      const mockData: CreateComputeToDataRequest = { did: 'testDid', algorithm: 'testAlgorithm' } as any;
+      const mockError = new Error('Something went wrong');
+
+      jest.spyOn(pontusxService, 'requestComputeToData').mockRejectedValue(mockError);
+
+      await expect(controller.runComputeToData(mockData)).rejects.toThrow(RpcException);
+    });
+  });
+
+  describe('getOffering', () => {
+    it('should return a did if an offering is found', async () => {
+      const mockData: GetOfferingRequest = { did: 'testDid' } as any;
+      const mockResult = { id: 'testDid' };
+
+      // TODO: Fix this test, but Asset is inaccessible
+
+      //jest.spyOn(pontusxService, 'getOffering').mockResolvedValue(mockResult);
+
+      //const result = await controller.getOffering(mockData);
+
+      //expect(result).toEqual({ did: mockResult.id });
+      //expect(pontusxService.getOffering).toHaveBeenCalledWith(mockData.did);
+    });
+
+    it('should throw an RpcException if no offering is found', async () => {
+      const mockData: GetOfferingRequest = { did: 'testDid' } as any;
+
+      jest.spyOn(pontusxService, 'getOffering').mockResolvedValue(null);
+
+      await expect(controller.getOffering(mockData)).rejects.toThrow(RpcException);
+    });
+  });
+
+  describe('getComputeToDataResult', () => {
+    it('should return "test-result"', async () => {
+      const mockData: CreateComputeToDataResultRequest = { JobId: "mockJobId" }
+      const mockResult: GetComputeToDataResultResponse = { JobId: "mockJobId", result: "test-result" };
+
+      jest.spyOn(pontusxService, 'getComputeToDataResult').mockResolvedValue(mockResult.result);
+
+      const result = await controller.getComputeToDataResult(mockData);
+
+      expect(result).toEqual(mockResult);
+      expect(pontusxService.getComputeToDataResult).toHaveBeenCalledWith(mockData.JobId);
+    });
   });
 });
