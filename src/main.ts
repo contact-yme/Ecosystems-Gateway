1ed2da2f32fd669e0cc3e9c799faf3f054903e1c
Sebastian Waldbauer <sebastian.waldbauer@posedio.com>
2024-09-30 10:10:57 +0200

    chore: linting

--- src/main.ts
+++ src/main.ts
@@ -12,8 +12,7 @@ async function bootstrap() {
   const configService = app.get<ConfigService>(ConfigService);
 
   const GRPC_BIND = configService.get('GRPC_BIND') || '0.0.0.0:5002';
-  const GRPC_GATEWAY_BIND =
-    configService.get('GRPC_GATEWAY_BIND') || 3000;
+  const GRPC_GATEWAY_BIND = configService.get('GRPC_GATEWAY_BIND') || 3000;
   const ENABLE_GRPC_REFLECTION =
     configService.get('ENABLE_GRPC_REFLECTION') || false;
   LOGGER.log(
