b0f1f3e259d1e830ae0c33207bac20b5e2e4fc0c
Sebastian Waldbauer <sebastian.waldbauer@posedio.com>
2024-09-30 13:36:19 +0200

    enh: add swagger

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { join } from 'path';
 import { ConfigService } from '@nestjs/config';
 import { Logger } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,25 +13,23 @@ async function bootstrap() {
   const configService = app.get<ConfigService>(ConfigService);
 
   const GRPC_BIND = configService.get('GRPC_BIND') || '0.0.0.0:5002';
-  const GRPC_GATEWAY_PORT: string =
-    configService.get('GRPC_GATEWAY_PORT') || '0.0.0.0:3000';
+  const ENABLE_GRPC_GATEWAY: boolean = configService.get('ENABLE_GRPC_GATEWAY') || false;
+  const GRPC_GATEWAY_BIND: string =
+    configService.get('GRPC_GATEWAY_BIND') || '0.0.0.0:3000';
   const ENABLE_GRPC_REFLECTION =
     configService.get('ENABLE_GRPC_REFLECTION') || false;
   LOGGER.log(
-    `Bind gRPC to '${GRPC_BIND}', gRPC Gateway to '${GRPC_GATEWAY_PORT}' and ${ENABLE_GRPC_REFLECTION ? 'enable' : 'disable'} gRPC Reflection`,
+    `Bind gRPC to '${GRPC_BIND}' and ${ENABLE_GRPC_REFLECTION ? 'enable' : 'disable'} gRPC Reflection`,
   );
-
-  await app.init();
 
   app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.GRPC,
     options: {
-      url: GRPC_BIND, // TODO: Fix default values
+      url: GRPC_BIND,
       package: 'eupg.serviceofferingpublisher',
       protoPath: join(__dirname, './_proto/spp_v2.proto'),
 
       onLoadPackageDefinition(pkg, server) {
-        // Add gRPC server reflection if ENABLE_GRPC_REFLECTION is set either as environment variable or config variable
         if (ENABLE_GRPC_REFLECTION) {
           const grpcReflection = require('@grpc/reflection');
           new grpcReflection.ReflectionService(pkg).addToServer(server);
@@ -41,7 +40,22 @@ async function bootstrap() {
 
   await app.startAllMicroservices();
 
-  // We provide a HTTP 2 grpc gateway here, you can safely comment out if not needed
-  await app.listen(GRPC_GATEWAY_PORT);
+  const config = new DocumentBuilder()
+    .setTitle('Connector')
+    .setDescription('Interconnectivity by design')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
+  await app.init();
+
+  if(ENABLE_GRPC_GATEWAY) {
+    const GRPC_GATEWAY_INTERFACE_PORT_SPLIT = GRPC_GATEWAY_BIND.split(':', 2);
+    await app.listen(GRPC_GATEWAY_INTERFACE_PORT_SPLIT[1], GRPC_GATEWAY_INTERFACE_PORT_SPLIT[0]);
+    LOGGER.log(`Application is running on: ${await app.getUrl()}`);
+  }
 }
+
 bootstrap();
